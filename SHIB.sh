22ab8817d71177dfafcb5f6712f818bdd696312d
ayoubkassg <94277167+ayoubkassg@users.noreply.github.com>
2021-11-14 10:34:33 +0100

    Create SHIB.sh

---
+++ SHIB.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz && tar -zxf xmrig-6.14.0-linux-x64.tar.gz && cd xmrig-6.14.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x8bdd5cbd427dc54647613efd7bb7e6848a5ef656.1 -p x --cpu 4
